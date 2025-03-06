09358cac3446f86bf7e8bcad4401d9e1499969aa
amadeodlp <amadeodlp@hotmail.com>
2025-03-05 22:22:13 -0300

    config changes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from '@/views/Home';
-import About from '@/views/About';
-import Skills from '@/views/Skills';
-import Projects from '@/views/Projects';
-import Contact from '@/views/Contact';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "@/views/Home"
+import About from "@/views/About"
+import Skills from "@/views/Skills"
+import Projects from "@/views/Projects"
+import Contact from "@/views/Contact"
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/skills" element={<Skills />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-  );
-};
+    <BrowserRouter basename="/amadeo-portfolio">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-export default AppRoutes;
+export default AppRoutes
