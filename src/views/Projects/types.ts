5bc8a7b61dd7cfe08e223812a7df384f40b9d0c7
amadeodlp <amadeodlp@hotmail.com>
2025-11-19 09:35:40 -0300

    fixes types

--- src/views/Projects/types.ts
+++ src/views/Projects/types.ts
@@ -1,13 +1,13 @@
 export interface ProjectsProps {}
 
 export interface Project {
-  id: string;
-  title: string;
-  description: string;
-  image: string;
-  technologies: string[];
-  github?: string;
-  liveDemo?: string;
-  featured?: boolean;
-  category: 'frontend' | 'backend' | 'fullstack' | 'mobile';
+  id: string
+  title: string
+  description: string
+  image: string
+  technologies: string[]
+  github?: string
+  liveDemo?: string
+  featured?: boolean
+  category: "frontend" | "backend" | "fullstack" | "mobile" | "ai" | "aws"
 }
